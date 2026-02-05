ad66e54712823f7de38240effe80f81357db29c5
Pnemo5 <elin.ly.wallgren@gmail.com>
2026-02-05 17:00:30 +0100

    Lektion 1
    Gjort seeder, index, skapat övriga filer som jag kommer arbeta i sen. Påbörjat show- och newfunktionen.

--- app.rb
+++ app.rb
@@ -15,9 +15,39 @@ class App < Sinatra::Base
       return @db
     end
 
-    # Routen /
     get '/' do
-        erb :index
+      redirect('/books')
+    end
+
+    get '/books' do
+      @books = db.execute('SELECT * FROM books')
+      p @books
+      erb(:"books/index")
+    end
+
+    get '/books/new' do
+      erb(:"books/new")
+    end
+
+    post '/books' do
+      p params
+      b_title = params["book_title"]
+      b_author = params["book_author"]
+      b_genre = params["book_genre"]
+      b_pages = params["book_pages"]
+      b_rating = params["book_rating"]
+      b_date = params["book_date"]
+      b_review = params["book_review"]
+      b_cover = params["book_cover"]
+
+      db.execute("INSERT INTO books (title, author, genre, pages, rating, date, review, cover)
+      VALUES(?, ?, ?, ?, ?, ?, ?, ?)", [b_title, b_author, b_genre, b_pages, b_rating, b_date, b_review, b_cover])
+      redirect("/books")
+    end
+
+    get '/books/:id' do | id |
+      @books = db.execute('SELECT * FROM books WHERE id=?', id)[0]
+      erb(:"books/show")
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,9 +1,58 @@
 require 'sqlite3'
+require_relative '../config'
 
 class Seeder
 
   def self.seed!
-    p "doit"
+    puts "Using db file: #{'db/DB_PATH'}"
+    puts "🧹 Dropping old tables..."
+    drop_tables
+    puts "🧱 Creating tables..."
+    create_tables
+    puts "🍎 Populating tables..."
+    populate_tables
+    puts "✅ Done seeding the database!"
+  end
+
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS books')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE books (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                author TEXT,
+                genre TEXT,
+                pages INTEGER,
+                rating INTEGER,
+                date TEXT,
+                review TEXT,
+                cover TEXT)')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO books (title, author, genre, pages, rating, date, review, cover) VALUES ("The Song of Achilles", "Madeline Miller", "roman", 408, 5, "januari", "SMÄRTA", "URL")')
+  end
+
+  private
+
+  def self.db
+    @db ||= begin
+      db = SQLite3::Database.new('db/books.sqlite.db')
+      db.results_as_hash = true
+      db
+    end
+  end
+
+  def self.db
+    @db ||= begin
+      db = SQLite3::Database.new(DB_PATH)
+      db.results_as_hash = true
+      db
+    end
   end
 
 end
+
+Seeder.seed!
